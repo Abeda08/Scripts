f6be95acb37561f9238282ac2a73d5be86f635bd
Abeda Banu <abeda2004@gmail.com>
2024-06-28 23:29:42 +0530

    Adding variable file to the Repo

---
+++ variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+name="John"
+echo "Hello , $name!"
+
+age=24
+echo "Age: $age years"
+
+x=4
+y=5
+sum=$((x+y))
+echo "Sum: $sum"
+
+Greeting="Hello"
+subject="World"
+
+message="$Greeting, $subject!"
+echo $message
